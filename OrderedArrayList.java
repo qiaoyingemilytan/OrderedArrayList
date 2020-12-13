4ff3248805676313f9661326f509205860356927
Emily <>
2020-12-13 02:58:19 -0500

    overriding set method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -2,7 +2,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   public int findIndex(T element){
     int index = 0;
-    for (int i = 0; i < super.size(); i++){
+    for (int i = 0; i < size(); i++){
       if(element.compareTo(get(index + 1)) > 0){
         index ++;
       }
@@ -25,4 +25,12 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super.add(findIndex(element), element);
   }
 
+  public T set(int index, T element){
+    if(element == null){
+      throw new IllegalArgumentException("Element is null");
+    }
+    remove(index);
+    add(element);
+    return get(index);
+  }
 }
